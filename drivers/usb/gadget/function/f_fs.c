2b96bd86c897c381af90bfccadbee566f0bf0ac0
Arian <arian.kulmer@web.de>
2022-03-15 23:38:17 +0100

    usb: f_fs: Fix ffs_log usage before ffs is set
    In order to avoid use-after-free the ffs_data struct
    is only set after spin lock since upstream commit
    32048f4be071f9a6966744243f1786f45bb22dc2 "usb: f_fs: Fix use-after-free for epfile".
    ffs_log tries to access ffs->ipc_log, which now fails
    because ffs is not set. Move the ffs_log usage behind
    the initialization of ffs to fix this issue.
    
    Change-Id: I471b7fa0116b4ddefd24e4413164923821dd7802

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -2087,6 +2087,11 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 	unsigned short count;
 	unsigned long flags;
 
+	spin_lock_irqsave(&func->ffs->eps_lock, flags);
+	count = func->ffs->eps_count;
+	epfile = func->ffs->epfiles;
+	ep = func->eps;
+
 	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
 		func->ffs->setup_state, func->ffs->flags);
 
@@ -2118,6 +2123,12 @@ static int ffs_func_eps_enable(struct ffs_function *func)
 	unsigned short count;
 	unsigned long flags;
 	int ret = 0;
+
+	spin_lock_irqsave(&func->ffs->eps_lock, flags);
+	ffs = func->ffs;
+	ep = func->eps;
+	epfile = ffs->epfiles;
+	count = ffs->eps_count;
 
 	ffs_log("enter: state %d setup_state %d flag %lu", func->ffs->state,
 		func->ffs->setup_state, func->ffs->flags);
